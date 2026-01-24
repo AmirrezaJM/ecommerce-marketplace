b5bc6fcca1088ccc7bc420f430bc8eeb9baa4298
amirrezajm <amirreza.jolani@yahoo.com>
2026-01-24 15:34:45 +0100

    feat: Set up shadcn/ui components, implement core layout, and establish initial page routing for the application.

---
+++ app/features/footer/footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+    return (
+        <footer className="flex justify-between font-medium p-6  border-t">
+            <div className="flex items-center gap-2 ">
+                <p className="text-center text-muted-foreground">
+                    &copy; 2026 Property, Inc. All rights reserved.
+                </p>
+            </div>
+        </footer>
+    );
+}
